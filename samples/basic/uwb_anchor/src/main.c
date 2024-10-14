26786c8ce141b75b00b04c60173c48cb5c5a2bf1
Hayden Goodwin <goodwinh8@gmail.com>
2024-10-14 16:01:35 +1000

    broken: bt_le_ext_adv_create fails

--- samples/basic/uwb_anchor/src/main.c
+++ samples/basic/uwb_anchor/src/main.c
@@ -9,6 +9,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
 
 #include <zephyr/drivers/ieee802154/deca_device_api.h>
 #include <zephyr/drivers/ieee802154/deca_interface.h>
@@ -26,25 +29,126 @@ LOG_MODULE_REGISTER(app);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct device *uwb = DEVICE_DT_GET(DT_INST(0, qorvo_dw3xxx));
 
-/* the anchor will be in a peripheral role which only maintains one active connection to the */
+/* The anchor will be in a peripheral role which only maintains one active connection to a tag.
+ * The anchor uses extended advertising to 
+ */
+
+/* ------------------------------------ Function Prototypes --------------------------------------- */
+static void connected(struct bt_conn *conn, uint8_t cnxn_err);
+static void disconnected(struct bt_conn *conn, uint8_t reason);
+static void start_adv(struct k_work *work);
+
+/* ------------------------------------ Bluetooth Vairables --------------------------------------- */
+static K_WORK_DEFINE(start_adv_worker, start_adv);
+
+/* Connection */
+static struct bt_conn *my_conn = NULL;
+static struct bt_conn_cb connection_callbacks = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+/* Advertising */
+static uint8_t mfg_data[] = { 0xff, 0xff, 0x00 };
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL|BT_LE_AD_NO_BREDR)),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_BAS_VAL)), /* Change this to the custom ranging servive when implemented*/
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+	BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, sizeof(mfg_data)), /* Placeholder for anchor location/gps coords */
+};
+
+/* Periodic advertising roughly every 211 ms */
+static const struct bt_le_adv_param adv_param = BT_LE_ADV_PARAM_INIT(
+	(BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CONNECTABLE ), 
+	0x0150, 0x0154, NULL
+);
+struct bt_le_ext_adv *adv;
 
 
+/* ------------------------------------ Bluetooth Functions --------------------------------------- */
+static void start_adv(struct k_work *work)
+{
+	int err;
+	err = bt_le_per_adv_start(adv);
+	if (err) {
+		LOG_ERR("Failed to start periodic advertising");
+	} else {
+		LOG_INF("Starting periodic advertising");
+	}
+
+	err = bt_le_ext_adv_start(adv, BT_LE_EXT_ADV_START_DEFAULT);
+	if (err) {
+		LOG_ERR("Failed to start extended advertising");
+	} else {
+		LOG_INF("Starting extended advertising");
+	}
+}
+
+static void connected(struct bt_conn *conn, uint8_t cnxn_err) 
+{
+	int err;
+	if (cnxn_err) {
+		LOG_ERR("Connection error: 0x%02x", cnxn_err);
+	} else {
+		LOG_INF("Connected");
+		my_conn = bt_conn_ref(conn);
+
+		err = bt_le_per_adv_stop(adv);
+		if (err) {
+			LOG_ERR("Failed to stop periodic advertising");
+		}
+		err = bt_le_ext_adv_stop(adv);
+		if (err) {
+			LOG_ERR("Failed to stop extended advertising");
+		}
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	LOG_INF("Disconnected. Reason: 0x%02x", reason);
+	bt_conn_unref(conn);
+	k_work_submit(&start_adv_worker);
+}
 
 int main(void)
 {
 	printk("Entering Main\n");
 	
-	int ret;
+	int err;
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
 	}	
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (err < 0) {
 		return 0;
 	}
-	
-	dw3xxx_configure_device(uwb, RANGING_RESPONDER, HRP_UWB_PHY_CHANNEL_9);
 
+	/* Enable bluetooth */
+	err = bt_enable(NULL);
+	if (err < 0) {
+		LOG_ERR("Bluetooth enable failed.");
+		return 0;
+	}
+	bt_conn_cb_register(&connection_callbacks);
+	
+	/* Create extended advertising set */
+	err = bt_le_ext_adv_create(&adv_param, NULL, &adv);
+	if (err) {
+		LOG_ERR("Failed to create advertising set (err %d)", err);
+		return 0;
+	}
+	err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err) {
+		LOG_ERR("Failed to set advertising data (err %d)", err);
+		return 0;
+	}
+
+	k_work_submit(&start_adv_worker);
+
+
+	dw3xxx_configure_device(uwb, RANGING_RESPONDER, HRP_UWB_PHY_CHANNEL_9);
+	/* Very important the the dw3000 irq is enabled after configuration */
 	dw_enable_irq(uwb);
 
 	while (1) {
